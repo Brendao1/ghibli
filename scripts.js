81241a8d189b6d0ce3b4221006d9635310d8b8d4
“Brenda” <odonnell.brenda@gmail.com>
2019-03-24 13:09:11 +0000

    each JSON item returned in the request is put into a 'card' element on the frontend. Creates 'card' 'h1' and 'p' elements and appends them to the frontend

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
+/* eslint-disable for-direction */
 // Create a request variable and assign a new XMLHttpRequest object to it.
+// eslint-disable-next-line getter-return
 var request = new XMLHttpRequest()
 
 // Open a new connection, using the GET request on the URL endpoint
@@ -10,9 +12,27 @@ request.onload = function () {
 // Begin accessing JSON data here
 var data = JSON.parse(this.response)
 
+// basically, if the request isn't 404 Not found, and is successful, then for each item in 
+// the JSON, put that item into a 'card' element, and display it in the container
 if (request.status >= 200 && request.status < 400 ){
 data.forEach(movie => {
-  console.log(movie.title)
+    const card = document.createElement('div')
+    card.setAttribute('class', 'card')
+
+    const h1 = document.createElement('h1')
+    h1.textContent = movie.title
+
+    const p = document.createElement('p')
+    movie.description = movie.description.substring(0, 300) // limit to 300 chars
+    p.textContent = `${movie.description}...` //end with an ellpisis
+
+    //append the cards to the container element
+    container.appendChild(card)
+
+    //each card will contain an h1 and a p
+    card.appendChild(h1)
+    card.appendChild(p)
+
 })
 } else {
     console.log('error')
