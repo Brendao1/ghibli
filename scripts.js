e8ac29f7cabe8e86d73706edd8c1427bcf207c5d
“Brenda” <odonnell.brenda@gmail.com>
2019-03-24 12:24:02 +0000

    puts back the code in scripts.js

--- scripts.js
+++ scripts.js
@@ -1,25 +1,25 @@
-// // Create a request variable and assign a new XMLHttpRequest object to it.
-// var request = new XMLHttpRequest()
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest()
 
-// // Open a new connection, using the GET request on the URL endpoint
-// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
 
-// request.onload = function () {
-//   // Begin accessing JSON data here
+request.onload = function () {
+  // Begin accessing JSON data here
 
-// // Begin accessing JSON data here
-// var data = JSON.parse(this.response)
+// Begin accessing JSON data here
+var data = JSON.parse(this.response)
 
-// if (request.status >= 200 && request.status < 400 ){
-// data.forEach(movie => {
-//   console.log(movie.title)
-// })
-// } else {
-//     console.log('error')
-// }
-// }
+if (request.status >= 200 && request.status < 400 ){
+data.forEach(movie => {
+  console.log(movie.title)
+})
+} else {
+    console.log('error')
+}
+}
 
-// request.send()
+request.send()
 
 const app = document.getElementById('root') // this is a div in the body of the index.html
 const logo = document.createElement('img') // this creates img element space in the DOM
