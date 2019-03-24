3a2c983b4088ff77b084e37685899fcd6429bdc4
“Brenda” <odonnell.brenda@gmail.com>
2019-03-24 12:09:51 +0000

    working on the DOM via the scripts.js to add an image which is displayed on the frontend of the site

--- scripts.js
+++ scripts.js
@@ -1,22 +1,30 @@
-// Create a request variable and assign a new XMLHttpRequest object to it.
-var request = new XMLHttpRequest()
+// // Create a request variable and assign a new XMLHttpRequest object to it.
+// var request = new XMLHttpRequest()
 
-// Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+// // Open a new connection, using the GET request on the URL endpoint
+// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
 
-request.onload = function () {
-  // Begin accessing JSON data here
+// request.onload = function () {
+//   // Begin accessing JSON data here
 
-// Begin accessing JSON data here
-var data = JSON.parse(this.response)
+// // Begin accessing JSON data here
+// var data = JSON.parse(this.response)
 
-if (request.status >= 200 && request.status < 400 ){
-data.forEach(movie => {
-  console.log(movie.title)
-})
-} else {
-    console.log('error')
-}
-}
+// if (request.status >= 200 && request.status < 400 ){
+// data.forEach(movie => {
+//   console.log(movie.title)
+// })
+// } else {
+//     console.log('error')
+// }
+// }
 
-request.send()
+// request.send()
+
+const app = document.getElementById('root') // this is a div in the body of the index.html
+const logo = document.createElement('img') // this creates img element space in the DOM
+logo.src = 'logo.png' //this sets the source of the image to be the pic we have on file
+const container = document.createElement('div') // this creates another div, container, to which we ascribe a class
+container.setAttribute('class', 'container') // we set the div's class as container
+app.appendChild(logo)  // this is how we place the logo and container into the app 'root'
+app.appendChild(container)
